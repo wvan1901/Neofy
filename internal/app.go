a925722f6958ccfbafd763a2f8a822aa6bd638e5
wvan1901 <wvan1901@gmail.com>
2024-09-12 22:44:22 -0500

    chore: clean up

--- internal/app.go
+++ internal/app.go
@@ -1,28 +1,24 @@
 package internal
 
 import (
-	"fmt"
 	"neofy/internal/config"
 	"neofy/internal/input"
 	"neofy/internal/output"
 	"neofy/internal/timer"
 )
 
-// NOTE: For our app we will have 3 panes:
-// Playlist(Left), Songs(Right), Bottom(footer)
 func RunApp() error {
 	d := config.InitAppData()
 	//d := config.InitMock()
 	defer d.Term.CloseTerminal()
-	fmt.Println("Access:", d.Spotify.UserTokens.AccessToken)
 	go d.Spotify.RefreshSchedular.Start()
-	u := timer.Updater{
+	songDataUpdater := timer.Updater{
 		Done:   make(chan bool),
 		Config: d,
 	}
-	go u.StartWithTimer()
+	go songDataUpdater.StartWithTimer()
 	for {
 		output.UpdateApp(d)
-		input.ProcessInput(d, &u)
+		input.ProcessInput(d, &songDataUpdater)
 	}
 }
